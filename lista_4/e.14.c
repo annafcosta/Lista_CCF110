cbf8980cfebd90799a6cfc4ffeb2a0f23e307e80
Anna Luísa <annalf1401@gmail.com>
2023-10-02 11:39:21 -0300

    Atualização dos exercícios

--- lista_4/e.14.c
+++ lista_4/e.14.c
@@ -8,43 +8,61 @@ int main()
     scanf("%d %d %d %d", &C, &D, &E, &F);
 
     // Verificar se o produto matricial é possível
-    if (D != E)
-    {
-        printf("\nImpossível");
-        return 0;
-    }
 
     int A[C][D], B[E][F], G[C][F];
 
-    for (int i = 0; i < C; i++)
-        for (int j = 0; j < D; j++)
-            scanf("%d", &A[i][j]);
-
-    for (int i = 0; i < E; i++)
-        for (int j = 0; j < F; j++)
-            scanf("%d", &B[i][j]);
-
-    // Calcular o produto matricial
-    for (int i = 0; i < C; i++)
+    if (C <= 6 && D <= 6 && E <= 6 && F <= 6)
     {
-        for (int j = 0; j < F; j++)
+        if (D != E)
         {
-            G[i][j] = 0;
-            for (int k = 0; k < D; k++)
+            printf("Impossivel");
+        }
+        else
+        {
+            for (int i = 0; i < C; i++)
             {
-                G[i][j] += A[i][k] * B[k][j];
+                for (int j = 0; j < D; j++)
+                {
+                    scanf("%d", &A[i][j]);
+                }
             }
-        }
-    }
 
-    for (int i = 0; i < C; i++)
-    {
-        for (int j = 0; j < F; j++)
-        {
-            printf("%d ", G[i][j]);
+
+            for (int i = 0; i < E; i++)
+            {
+                for (int j = 0; j < F; j++)
+                {
+                    scanf("%d", &B[i][j]);
+                }
+            }
+
+            // Calcular o produto matricial
+            for (int i = 0; i < C; i++)
+            {
+                for (int j = 0; j < F; j++)
+                {
+                    G[i][j] = 0;
+
+                    for (int k = 0; k < D; k++)
+                    {
+                        G[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+
+            for (int i = 0; i < C; i++)
+            {
+                for (int j = 0; j < F; j++)
+                {
+                    printf("%d ", G[i][j]);
+                }
+                printf("\n");
+            }
+
         }
-        printf("\n");
+
     }
 
     return 0;
 }
+
