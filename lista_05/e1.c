9bfb0637cc715e82c2ad0904fa38940126952d20
Anna Luísa <annalf1401@gmail.com>
2023-11-06 10:49:37 -0300

    att

---
+++ lista_05/e1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct informacoes
+{
+    char nome[50];
+    char endereco[50];
+    int numero;
+} tipo_informacoes;
+
+int main(){
+
+
+    tipo_informacoes end;
+    tipo_informacoes informacoes[5];
+
+    int i = 0;
+
+    while (i <= 4)
+    {
+        scanf("%s", &end.nome);
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
