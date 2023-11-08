d0ef85092a640c1d75b73180c56efa87840322c5
Anna Luísa <annalf1401@gmail.com>
2023-11-07 21:56:44 -0300

    att

---
+++ lista_05/e5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef struct {
+    char nome;
+    int matricula;
+    int notas;
+} Alunos;
+
+int main() {
+
+    Alunos aluno[5];
+    int i;
+
+    for (i = 0; i < 3; i++)
+    {
+        scanf("%s", aluno[i].nome);
+        scanf("%d", &aluno[i].matricula);
+        scanf("%d", &);
+    }
+
+   
+    return 0;
+}
